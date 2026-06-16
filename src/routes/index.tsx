facb10ee632b116a990249a85ec9c390aabe2ef9
Gavin Power <me@gavinpower.dev>
2026-06-15 23:00:33 -0230

    rsc

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,29 @@
 import { createFileRoute } from "@tanstack/react-router";
+import { createServerFn } from "@tanstack/react-start";
+import { renderServerComponent } from "@tanstack/react-start/rsc";
 
-export const Route = createFileRoute("/")({ component: App });
+function RscIntro() {
+	return <p>This paragraph was rendered as a React Server Component.</p>;
+}
+
+const getRscIntro = createServerFn().handler(async () => {
+	const Intro = await renderServerComponent(<RscIntro />);
+
+	return { Intro };
+});
+
+export const Route = createFileRoute("/")({
+	loader: async () => getRscIntro(),
+	component: App,
+});
 
 function App() {
+	const { Intro } = Route.useLoaderData();
+
 	return (
 		<main>
 			<h1>Close By</h1>
+			{Intro}
 		</main>
 	);
 }
--- vite.config.ts
+++ vite.config.ts
@@ -4,15 +4,26 @@ import tailwindcss from "@tailwindcss/vite";
 import { devtools } from "@tanstack/devtools-vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact, { reactCompilerPreset } from "@vitejs/plugin-react";
+import rsc from "@vitejs/plugin-rsc";
 import { defineConfig } from "vite";
 
 const config = defineConfig({
 	resolve: { tsconfigPaths: true },
 	plugins: [
 		devtools(),
-		cloudflare({ viteEnvironment: { name: "ssr" } }),
+		cloudflare({
+			viteEnvironment: {
+				name: "ssr",
+				childEnvironments: ["rsc"],
+			},
+		}),
 		tailwindcss(),
-		tanstackStart(),
+		tanstackStart({
+			rsc: {
+				enabled: true,
+			},
+		}),
+		rsc(),
 		viteReact(),
 		babel({ presets: [reactCompilerPreset()] }),
 	],
