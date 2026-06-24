60cb4bc4d63d3224f3224925dc6a72689dce45f9
Gavin Power <me@gavinpower.dev>
2026-06-24 16:10:24 -0230

    add ncu script, use nicer script names, update packages

--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -2,6 +2,6 @@ import { env } from "cloudflare:workers";
 import { drizzle } from "drizzle-orm/d1";
 import * as schema from "@/lib/db/schema";
 
-export const db = drizzle(env.d1, { schema });
+const db = drizzle(env.d1, { schema });
 
-export { schema };
+export { db, schema };
